c5d3b4e35a1c20afedcd954fed71ff21b8e67ecf
sai kumar <saiyadav27398@gmail.com>
2022-03-05 18:38:54 +0530

    app.js

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout/Checkout";
+import Login from "./Login/Login";
+import { useStateValue } from "./StateProvider";
+
+function App() {
+  //Data layer component that connected to firebase
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    //Listener
+    // will only run once when the app component loads.., its similar to if statement in react
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+      if (authUser) {
+        //The user just logged in/ the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
